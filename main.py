03d7f9c15c232b73aa3431290eb3e80518309bbc
Alexander Ushakov <ikhemul@gmail.com>
2019-08-20 23:20:57 +0400

    Работа с папками

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import os
+import shutil
+
+path_save_folder = r"D:\Python\Temp\runic games\torchlight 2\save"
+path_finish_folder = os.getcwd() + r"\temp"
+
+shutil.copytree(path_save_folder, path_finish_folder)
+
+print(os.listdir(path_save_folder))
 
