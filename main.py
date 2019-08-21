871527e61c81366b23f3919ac52723314ea3e06e
Alexander Ushakov <ikhemul@gmail.com>
2019-08-21 20:22:34 +0400

    Add command for wokr

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
 import shutil
+import datetime
 
-path_save_folder = r"D:\Python\Temp\runic games\torchlight 2\save"
-path_finish_folder = os.getcwd() + r"\temp"
+cmd = input('>>>> ')
 
-shutil.copytree(path_save_folder, path_finish_folder)
+# timestamp = datetime.datetime.today().strftime("%Y%m%d_%H_%M_%S")
 
-print(os.listdir(path_save_folder))
+# path_save_folder = r"D:\Python\Temp\runic games\torchlight 2\save"
+# path_finish_folder = os.getcwd() + "\\" + timestamp
+
+# shutil.copytree(path_save_folder, path_finish_folder)
+#
+# print(os.listdir(path_finish_folder))
+
+if cmd == 'save' :
+    print('save')
+elif cmd == 'back' :
+    print('back')
+else:
+    print('error')
 
