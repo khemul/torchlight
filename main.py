ca334a01432354be3e5502dc8483dfe769dc9d2d
Alexander Ushakov <ikhemul@gmail.com>
2019-08-22 22:06:20 +0400

    Add function copy_save

--- main.py
+++ main.py
@@ -12,11 +12,29 @@ cmd = input('>>>> ')
 # shutil.copytree(path_save_folder, path_finish_folder)
 #
 # print(os.listdir(path_finish_folder))
+path_save_folder = r'D:\Python\Temp\runic games\torchlight 2\save'
+
+
+
+def copy_save(path_start):
+
+    timestamp = datetime.datetime.today().strftime("%Y%m%d_%H_%M_%S")
+    path_finish_folder = os.path.join(os.getcwd(), 'temp', timestamp)
+    print(os.listdir(os.path.join(os.getcwd(), 'temp')))
+    shutil.copytree(path_start, path_finish_folder)
+    return print('Done!')
+
+def back_save():
+
+
+
 
 if cmd == 'save' :
-    print('save')
+    copy_save(path_save_folder)
+    # print(os.listdir(os.getcwd() + os.path.join('\temp')))
 elif cmd == 'back' :
     print('back')
 else:
     print('error')
+
 
